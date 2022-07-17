9bcd9ccc0c0b1dc15026a0ca8641fdeea12cc026
tochyodikwa <odikwatochy233@gmail.com>
2022-07-17 04:14:37 -0700

    First commit android x

---
+++ app/src/main/java/com/tochycomputerservices/sample/SecondActivity.java
@@ -0,0 +1,20 @@
+package com.tochycomputerservices.sample;
+
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+/**
+ * Created by Eze-Odikwa Tochukwu Jed on 2/7/22
+ * as a practical example for REST API
+ * Attribute to me, it helps a dog find his bone wink
+ */
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
